52c294c63ba1f53f130960673f4ad88b73abd446
dnahstar <dnahstar@gmail.com>
2026-03-31 17:11:20 +0900

    Update resizable.tsx

--- components/ui/resizable.tsx
+++ components/ui/resizable.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import { GripVertical } from "lucide-react"
 import * as ResizablePrimitive from "react-resizable-panels"
+import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
@@ -36,7 +36,7 @@ const ResizableHandle = ({
   >
     {withHandle && (
       <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
-        <GripVertical className="h-2.5 w-2.5" />
+        <div className="h-2.5 w-0.5 bg-foreground/20" />
       </div>
     )}
   </ResizablePrimitive.PanelResizeHandle>
