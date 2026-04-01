6ea1bb8ac6765004514b16a6b02cec9a255b6605
dnahstar <dnahstar@gmail.com>
2026-04-02 02:09:57 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // 정적 배포를 위해 반드시 필요
+  basePath: '/pioneers-ring-catcher', // 상세 주소 경로를 인식하게 만드는 핵심!
   images: {
-    unoptimized: true, // 이미지 최적화 에러 방지 (핵심!)
+    unoptimized: true, // 이미지 최적화 에러 방지
   },
-  // 경로가 꼬이는 걸 방지하기 위해 추가
-  trailingSlash: true,
+  trailingSlash: true, // 경로가 꼬이는 걸 방지하기 위해 유지
 };
 
 export default nextConfig;
