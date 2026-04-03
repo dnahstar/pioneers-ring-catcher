81363db198b4d2aba9a796cad15ab50ce9162c7b
dnahstar <dnahstar@gmail.com>
2026-04-04 05:53:19 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,11 @@
 import "./globals.css";
 import Script from "next/script";
 
-export const metadata = {
-  title: "PIONEERS!! - Ring Catcher 2.0",
-  description: "Grit leads to the Mainnet",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <head>
-        {/* strategy="beforeInteractive"를 써야 팝업 차단을 피할 수 있습니다 */}
+        {/* 파이 SDK를 가장 먼저 로드합니다 */}
         <Script 
           src="https://sdk.minepi.com/pi-sdk.js" 
           strategy="beforeInteractive"
