662179c48be4bc3d4b88c4c15bfcca41cf6be626
dnahstar <dnahstar@gmail.com>
2026-04-05 05:28:26 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // 정적 사이트 배포용 (GitHub Pages)
-  images: {
-    unoptimized: true, // 이미지 최적화 끄기 (빌드 에러 방지)
-  },
-  // 필요한 경우 basePath: '/프로젝트이름' 추가
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '', 
+  assetPrefix: '',
 };
-
 export default nextConfig;
